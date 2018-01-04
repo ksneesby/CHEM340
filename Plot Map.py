84c4fa9387ac4ee1b0b4fa042945ad4ad0374114
Kate Sneesby <kls441@uowmail.edu.au>
2018-01-05 00:40:43 +1100

    Code will now plot some points on map

--- Plot Map.py
+++ Plot Map.py
@@ -13,28 +13,30 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as pyplot
 import numpy
 
-df = pd.read_csv('/home/kate/Downloads/ORCASall.mergeTOGA.tbl', sep=' ', na_values = "-888") #Setting values outside detection limit as NA values
-
-lon,lat= df["LONC"],df["LATC"]
-
-lons = lon[216]
-lats = lat[216]
-dats = data[216]
-
-data = df["Isoprene_TOGA"]
-
-map = Basemap(projection='robin',lon_0=-70,lat_0=0)
-
-map.drawcoastlines(color = 'black')
-map.drawcountries(color='black')
+#df = pd.read_csv('/home/kate/Downloads/ORCASall.mergeTOGA.tbl', sep=' ', na_values = "-888") #Setting values outside detection limit as NA values
+df = pd.read_csv('C:\Users\Kate\Documents\CHEM340repository\CHEM340\ORCASall.mergeTOGA.tbl', sep=' ', na_values = "-888") #Setting values outside detection limit as NA values
 
 
-x, y = map(lons, lats)
-
-map.scatter(lons, lats)
-
-
-216-221
+lon = df["LONC"].values
+lat = df["LATC"].values
+data = df["Isoprene_TOGA"].values
 
 
-pyplot.show()
+
+
+# 1. Draw the map background
+fig = plt.figure(figsize=(8, 8))
+m = Basemap(projection='robin',lon_0=-70,lat_0=0)
+
+
+m.drawcoastlines(color='black')
+m.drawcountries(color='black')
+
+
+# 2. scatter city data, with color reflecting population
+# and size reflecting area
+m.scatter(lon, lat, latlon=True, c=data, cmap='RdBu_r', alpha=0.5)
+
+# 3. create colorbar and legend
+plt.colorbar(label=r'Isoprene')
+plt.clim(1, 744)
