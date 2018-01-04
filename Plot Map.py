927add7f87411799506a176281c358c753c5ef7e
Kate Sneesby <kls441@uowmail.edu.au>
2018-01-05 10:42:51 +1100

    Changed scatter comment to reflect my project and changed colour of plotted points

--- Plot Map.py
+++ Plot Map.py
@@ -13,8 +13,8 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as pyplot
 import numpy
 
-#df = pd.read_csv('/home/kate/Downloads/ORCASall.mergeTOGA.tbl', sep=' ', na_values = "-888") #Setting values outside detection limit as NA values
-df = pd.read_csv('C:\Users\Kate\Documents\CHEM340repository\CHEM340\ORCASall.mergeTOGA.tbl', sep=' ', na_values = "-888") #Setting values outside detection limit as NA values
+df = pd.read_csv('/home/kate/Downloads/ORCASall.mergeTOGA.tbl', sep=' ', na_values = "-888") #Setting values outside detection limit as NA values
+#df = pd.read_csv('C:\Users\Kate\Documents\CHEM340repository\CHEM340\ORCASall.mergeTOGA.tbl', sep=' ', na_values = "-888") #Setting values outside detection limit as NA values
 
 
 lon = df["LONC"].values
@@ -25,7 +25,7 @@ data = df["Isoprene_TOGA"].values
 
 
 # 1. Draw the map background
-fig = plt.figure(figsize=(8, 8))
+fig = pyplot.figure(figsize=(8, 8))
 m = Basemap(projection='robin',lon_0=-70,lat_0=0)
 
 
@@ -33,10 +33,9 @@ m.drawcoastlines(color='black')
 m.drawcountries(color='black')
 
 
-# 2. scatter city data, with color reflecting population
-# and size reflecting area
-m.scatter(lon, lat, latlon=True, c=data, cmap='RdBu_r', alpha=0.5)
+# 2. scatter Isoprene data
+m.scatter(lon, lat, latlon=True, c=data, cmap=pyplot.get_cmap('viridis'), s=16)
 
 # 3. create colorbar and legend
-plt.colorbar(label=r'Isoprene')
-plt.clim(1, 744)
+pyplot.colorbar(label=r'Isoprene')
+pyplot.clim(1, 744)
